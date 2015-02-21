97a0a9c187d500e68ce7dd60872ec82b7f5f0699
Gautam Kishore <gautam@eulogik.com>
2015-02-21 13:02:24 +0530

    with options, split words

--- js/wordswitch.jquery.js
+++ js/wordswitch.jquery.js
@@ -1,12 +1,33 @@
 // JQuery plugin for switching word
+var lert = function(d){console.log(d)};
 (function ( $ ) {
-$.fn.wordswitch = function() {
+$.fn.wordswitch = function(options) {
+	var settings = $.extend({
+            // These are the defaults.
+            delay: "3s",
+            separator: ","
+        }, options );
 	return this.each(function() {
 		//logics here
-		this.css( "color", "green" );
+		var h = "";
+		var $t = $(this);
+		var words = $t.html().split(settings.separator);
+		$.each( words, function( key, value ) {
+			value = $.trim(value);
+		  lert( key + ": " + value );
+		  
+		});
+		//lert(words);
 		return this;
 	});
 };
 }( jQuery ));
- 
+
+
+
+
+
 $( ".wordswitch" ).wordswitch(); // Makes all the links green.
+
+
+

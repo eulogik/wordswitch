8e826be1e57704c88a5dce806a106c80462a0019
Gautam Kishore <gautam@eulogik.com>
2015-02-21 14:55:21 +0530

    first working draft
    with just 1 type of animation.
    some bug left though, have to manage keyframes based on each element
    dynamically

--- js/wordswitch.jquery.js
+++ js/wordswitch.jquery.js
@@ -4,28 +4,31 @@ var lert = function(d){console.log(d)};
 $.fn.wordswitch = function(options) {
 	var settings = $.extend({
             // These are the defaults.
-            delay: "3s",
+            delay: 1,
             separator: ","
         }, options );
+	var switcherCounts = 0;
 	return this.each(function() {
 		//logics here
-		var h = "";
+		switcherCounts+=1;
+		var h = '<div class="ws-words ws-words-1">';//html to replace from
 		var $t = $(this);
 		var words = $t.html().split(settings.separator);
-		$.each( words, function( key, value ) {
-			value = $.trim(value);
-		  lert( key + ": " + value );
-		  
+		var d = 0; //delay increased for each element
+		var c = settings.delay * words.length; // total cycle of rotation
+		$.each( words, function( k, v ) {
+			value = $.trim(v);
+		  //lert( k + ": " + v );
+		  h+='<span style="-webkit-animation-delay: '+d+'s;-ms-animation-delay: '+d+'s; animation-delay: '+d+'s; -webkit-animation-duration: '+c+'s;">'+v+'</span>';
+		  d+=settings.delay;
 		});
+		h+='</div>';
+		$t.html(h);
 		//lert(words);
 		return this;
 	});
 };
 }( jQuery ));
-
-
-
-
 
 $( ".wordswitch" ).wordswitch(); // Makes all the links green.
 
